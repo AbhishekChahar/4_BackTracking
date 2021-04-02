a124846f0ce957f2b249c8e2369a0c5eaf04ec35
Abhishek Chahar <>
2021-04-02 13:53:29 +0000

    Initial commit

---
+++ ac.cpp
@@ -0,0 +1,27 @@
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+
+// int main(){
+
+//   // vector<bool>nums(8,true);
+
+//   bool arr[8] ={true};
+//   fill_n(arr,8,true);
+//   // int n;
+//   // cin>>n;
+//   // for(int i =0; i<n ;i++){
+//   //   int data;
+//   //   cin>>data;
+//   //   nums.push_back(data);
+//   // }
+
+//   for(auto x: arr){
+//     cout<<x<<" ";
+//   }
+//   cout<<endl;
+
+//   return 0;
+// }
